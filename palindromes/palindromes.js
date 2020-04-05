949ee81c434c8fbe2e3192cc4b5018cddb8ba620
NotNullDev <notnulldev0@gmail.com>
2020-04-05 09:44:05 +0200

    done palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function() {
-
+const palindromes = function(wordString) {
+    const string = wordString.replace(/\W/g, "").toLowerCase();
+    const arrayOfChars = Array.from(string);
+    const reversedArray = [...arrayOfChars.reverse()]
+    const reversedString = reversedArray.join("");
+    if(string === reversedString){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 module.exports = palindromes
