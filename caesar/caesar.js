786d0227ae2e99dd2bf513f8af9d3dec9f40df0a
NotNullDev <notnulldev0@gmail.com>
2020-04-05 11:05:23 +0200

    done caesar exercise

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,26 @@
-const caesar = function() {
+const caesar = function(word, lettersToMove) {
+    return Array.from(word)
+    .map(letter => {
 
+        if(/\W/.test(letter)){
+            return letter;
+        }
+
+        let letterCode = letter.charCodeAt();
+        lettersToMove %= 26;
+        let shiftLetter = letterCode + lettersToMove;
+    
+        if((letterCode <= 90 && shiftLetter > 90) || (letterCode > 90 && shiftLetter> 122)){
+            shiftLetter -= 26
+        }else if((letterCode <= 90 && shiftLetter < 65) || (letterCode > 90 && shiftLetter < 90)){
+            
+            shiftLetter += 26
+        }
+
+        let newLetter = String.fromCharCode(shiftLetter);
+        return newLetter;
+    })
+    .join("")
 }
 
 module.exports = caesar
