556670626446fdf48e6f2f5ecd90aa902e94ea39
NotNullDev <notnulldev0@gmail.com>
2020-04-05 11:43:27 +0200

    done fubonacii exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(number1) {
+    if(number1 < 0){
+        return "OOPS";
+    }
+  const fibonacciArray = [];
+  for(i = 0; i< number1; i++){
+    if(i === 0 || i === 1 ){
+        fibonacciArray.push(1);
+        continue;
+    }
+    let nextNumber = fibonacciArray[i -2] + fibonacciArray[i -1];
+    fibonacciArray.push(nextNumber)
+  }
+  return fibonacciArray.pop();
 }
 
 module.exports = fibonacci
