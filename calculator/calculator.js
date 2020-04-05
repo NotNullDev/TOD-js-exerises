2b2a3229f38bc824b3f12bcbecca4d6e612e61ed
NotNullDev <notnulldev0@gmail.com>
2020-04-05 08:32:24 +0200

    calc exercise is done

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,44 @@
-function add () {
+function add (number1, number2) {
+	return number1 + number2;
+}
+
+function subtract (number1, number2) {
+	return number1 - number2;
+}
+
+function sum (arrayOfNumbers) {
+	return arrayOfNumbers.reduce((sum,number)=>{
+		sum += number;
+		return sum;
+	}, 0);
 	
 }
 
-function subtract () {
-	
+function multiply (arrayOfNumbers) {
+	if(arrayOfNumbers.length === 0){
+		return;
+	}
+	return arrayOfNumbers.reduce((multiply, number)=>{
+		return multiply * number;
+	}, 1)
 }
 
-function sum () {
-	
+function power(number1, number2) {
+	return number1**number2;
 }
 
-function multiply () {
-	
-}
-
-function power() {
-	
-}
-
-function factorial() {
-	
+function factorial(number1) {
+	if(number1 == 0){
+		return 1;
+	}else if(number1> 0 && Number.isInteger(number1)){
+		let factorial = 1;
+		for(let i = 1; i <= number1; i++){
+			factorial *= i; 
+		}
+		return factorial;
+	}else{
+		return;
+	}
 }
 
 module.exports = {
